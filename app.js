1512c5313c42eb1afb662071a4684f5e24bb0239
gopalbharadva <46951254+gopalbharadva@users.noreply.github.com>
2020-12-12 20:20:26 +0530

    minions translator doen

--- app.js
+++ app.js
@@ -2,7 +2,22 @@ var btn=document.querySelector("#click-translate");
 var input=document.querySelector("#text-input");
 var output=document.querySelector("#output");
 
-function out(){
-    output.innerText=input.value;
+var serverurl="https://api.funtranslations.com/translate/minion.json"
+
+function geturl(inputvalue)
+{
+    return serverurl+"?"+"text="+inputvalue;
+}
+function errorhandler(error)
+{
+    alert("Sorry,Error occurred!!")
+}
+function out()
+{
+    var inputstring=input.value;
+    fetch(geturl(inputstring)).
+    then(response => response.json()).
+    then(json =>console.log(output.innerText=json.contents.translated)).
+    catch(errorhandler);
 }
 btn.addEventListener("click",out)
